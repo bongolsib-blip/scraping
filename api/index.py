25fc84167397a8a22ee90e2635f70abc3cdd2793
bongolsib-blip <bongolsib@gmail.com>
2026-03-12 11:00:18 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,51 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
+from http.server import BaseHTTPRequestHandler
+import json
 
-def handler(request):
+class handler(BaseHTTPRequestHandler):
 
-    keywords = ["asus", "hp", "lenovo", "msi"]
-    rows = []
+    def do_GET(self):
 
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
+        keywords = ["asus", "hp", "lenovo", "msi"]
+        rows = []
 
-    for i in keywords:
+        headers = {
+            "User-Agent": "Mozilla/5.0"
+        }
 
-        url = f"https://els.id/product-category/laptop/laptop-by-brand/{i}-laptop-by-brand-laptop-default-category/?per_page=1000"
+        for i in keywords:
 
-        response = requests.get(url, headers=headers)
+            url = f"https://els.id/product-category/laptop/laptop-by-brand/{i}-laptop-by-brand-laptop-default-category/?per_page=1000"
 
-        if response.status_code == 200:
+            response = requests.get(url, headers=headers)
 
-            soup = BeautifulSoup(response.text, "html.parser")
-            products = soup.find_all(class_="product-info")
+            if response.status_code == 200:
 
-            for product in products:
-                try:
-                    name = product.find(class_="product-title").text.strip()
-                    price = product.find(class_="woocommerce-Price-amount amount").text.strip()
+                soup = BeautifulSoup(response.text, "html.parser")
+                products = soup.find_all(class_="product-info")
 
-                    price = price.replace("Rp", "").replace(",", "").replace(".00", "").strip()
-                    price = int(price)
+                for product in products:
+                    try:
+                        name = product.find(class_="product-title").text.strip()
+                        price = product.find(class_="woocommerce-Price-amount amount").text.strip()
 
-                    rows.append({
-                        "toko": "ELS",
-                        "sku": i,
-                        "nama_produk": name,
-                        "harga": price
-                    })
+                        price = price.replace("Rp", "").replace(",", "").replace(".00", "").strip()
+                        price = int(price)
 
-                except:
-                    pass
+                        rows.append({
+                            "toko": "ELS",
+                            "sku": i,
+                            "nama_produk": name,
+                            "harga": price
+                        })
 
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": {
+                    except:
+                        pass
+
+        result = {
             "total": len(rows),
             "data": rows
         }
-    }
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(result).encode())
